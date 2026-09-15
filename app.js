e77e2f8e6fb4171344b86ec47977540db6b22f8a
fengyu <3483718522@qq.com>
2026-09-15 12:06:28 +0800

    避免每个地点都重复写代码，打算更新地点数据类，在此存旧档

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 
 
-const map = L.map("map").setView([43.88, 125.30], 14);
+// const map = L.map("map").setView([43.88, 125.30], 17.5);
+const map = L.map("map").setView([43.82048, 125.26893], 17.5);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "&copy; OpenStreetMap"
-}).addTo(map);
+
+// L.tileLayer(
+//   "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
+//   {
+//     attribution: "Tiles &copy; Esri"
+//   }
+// ).addTo(map);
+
+L.tileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  {
+    attribution: "Tiles &copy; Esri",
+    maxZoom: 19
+  }
+).addTo(map);
+
+
+const marker = L.marker([43.82048, 125.26893]).addTo(map);
+marker.bindPopup("吉林大学前卫南区操场");
+
+const marker0 = L.marker([43.822611, 125.263206]).addTo(map);
+marker0.bindPopup("鼎新图书馆");
+
+const marker1 = L.marker([43.817974, 125.263345]).addTo(map);
+marker1.bindPopup("宋志平体育馆");
+
+map.on("click", (event) => {
+  const latitude = event.latlng.lat;
+  const longitude = event.latlng.lng;
+
+  console.log(latitude, longitude);
+
+  L.popup()
+    .setLatLng(event.latlng)
+    .setContent(`${latitude.toFixed(6)}, ${longitude.toFixed(6)}`)
+    .openOn(map);
+});
 
 
 const places = [
